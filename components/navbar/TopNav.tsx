84490931368001727bf0bd42be63e54dd8d0b6e0
Dana Sharon <whosedreamisthis@gmail.com>
2026-02-25 12:22:16 -0800

    active tab

--- components/navbar/TopNav.tsx
+++ components/navbar/TopNav.tsx
@@ -1,9 +1,21 @@
+'use client';
+
 import React from 'react';
 import Logo from './Logo';
 import Link from 'next/link';
 import ProfileButton from './ProfileButton';
+import { usePathname } from 'next/navigation';
+import { cn } from '@/lib/utils';
+
+const navItems = [
+	{ name: 'Members', href: '/members' },
+	{ name: 'Lists', href: '/lists' },
+	{ name: 'Messages', href: '/messages' },
+];
 
 export default function TopNav() {
+	const pathname = usePathname();
+
 	return (
 		<nav className="sticky top-0 z-50 w-full border-b border-rose-100 bg-gradient-to-r from-rose-200/80 via-white to-rose-100/80 backdrop-blur-md">
 			<div className="flex h-16 items-center justify-between px-4">
@@ -13,15 +25,29 @@ export default function TopNav() {
 					<Logo />
 				</Link>
 				<div className="flex gap-3">
-					<Link href="/members" className="uppercase">
-						Matches
-					</Link>
-					<Link href="/lists" className="uppercase">
-						Lists
-					</Link>
-					<Link href="/messages" className="uppercase">
-						Messages
-					</Link>
+					{navItems.map((item) => {
+						const isActive = pathname === item.href;
+
+						return (
+							<Link
+								key={item.href}
+								href={item.href}
+								className={cn(
+									'relative uppercase flex h-full items-center text-sm font-medium transition-colors hover:text-rose-500',
+									isActive
+										? 'text-rose-600'
+										: 'text-slate-500',
+								)}
+							>
+								{item.name}
+
+								{/* The Active Indicator (Underline) */}
+								{/* {isActive && (
+									<div className="absolute bottom-0 left-0 h-[2px] w-full bg-gradient-to-r from-rose-500 to-orange-400" />
+								)} */}
+							</Link>
+						);
+					})}
 				</div>
 				<ProfileButton />
 			</div>
