be514992339164e1fcb7ffe7bce3ffe67466117b
Dana Sharon <whosedreamisthis@gmail.com>
2026-02-26 11:09:55 -0800

    Member image in member bottom bar

--- components/MemberBottomBar.tsx
+++ components/MemberBottomBar.tsx
@@ -2,7 +2,8 @@ import { Member } from '@/lib/generated/prisma/client';
 import React from 'react';
 import { Card } from './ui/card';
 import Link from 'next/link';
-
+import Image from 'next/image';
+import { Divide } from 'lucide-react';
 type Props = {
 	member: Member;
 };
@@ -15,7 +16,18 @@ export default function MemberBottomBar({ member }: Props) {
 	];
 
 	return (
-		<div className="sticky bottom-0 flex flex-row justify-between w-full mt-10 items-center">
+		<nav className="sticky bottom-0 flex flex-row justify-between w-full mt-10 items-center px-4 py-1 items-end">
+			<div className="flex items-center gap-2">
+				<div className="relative h-10 w-10 overflow-hidden rounded-full border-2 border-rose-500">
+					<Image
+						src={member.image || '/images/user.png'}
+						alt={member.name}
+						fill
+						className="object-cover"
+					/>
+				</div>
+				<span className="font-semibold text-sm">{member.name}</span>
+			</div>
 			{navLinks.map((nl, index) => {
 				return (
 					<Link className="mx-4 my-2" key={index} href={nl.href}>
@@ -23,6 +35,6 @@ export default function MemberBottomBar({ member }: Props) {
 					</Link>
 				);
 			})}
-		</div>
+		</nav>
 	);
 }
